9431066572f206dd39a5904875dba2ad4bbb99c4
Artyom Avetisyan <artyom.avetisyan@gmail.com>
2016-08-15 12:21:02 +0200

    .net framework 4.5 commit

---
+++ src/MongoLab/Program.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoLab
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var mongoClient = new MongoClient();
+            var dbList = mongoClient.ListDatabases();
+        }
+    }
+}
