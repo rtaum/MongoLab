67b587b9eceec10f41e6ea1d1369a8cdb1395d7c
Artyom Avetisyan <artyom.avetisyan@gmail.com>
2016-08-16 21:53:34 +0200

    some changes

--- src/MongoLab/Program.cs
+++ src/MongoLab/Program.cs
@@ -1,4 +1,6 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,30 @@ namespace MongoLab
         public static void Main(string[] args)
         {
             var mongoClient = new MongoClient();
-            var dbList = mongoClient.ListDatabases();
+            var students = mongoClient.GetDatabase("students");
+            var sort = new BsonDocument
+            {
+                { "student_id", 1 },
+                { "score", 1 }
+            };
+
+            var studentsValue = students.
+                GetCollection<Students>("grades").
+                Find(s => s.type == "homework").
+                Sort(sort).
+                ToEnumerable();
+
+            bool skip = true;
+            foreach (var s in studentsValue)
+            {
+                skip = !skip;
+                if (skip)
+                {
+                    continue;
+                }
+
+                students.GetCollection<Students>("grades").DeleteOne<Students>(one => one._id == s._id);
+            }
         }
     }
 }
---
+++ src/MongoLab/Students.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoLab
+{
+    public class Students
+    {
+        public ObjectId _id { get; set; }
+        public int student_id { get; set;  }
+
+        public string type { get; set; }
+
+        public double score { get; set; }
+    }
+}
